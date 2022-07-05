b39c650ef32de15bee971432e5d06b92df125c1b
CristoforosChoras <christopher.choras@yahoo.com>
2022-07-05 10:07:34 +0200

    nab bar styled components

---
+++ src/components/navbar/index.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const NavBar = () => {
+  return (
+    <>
+        <Nav>
+            <NavBarContainer>
+                <NavLogo>blabla</NavLogo>
+            </NavBarContainer>
+        </Nav>
+    </>
+  )
+}
+
+export default NavBar
