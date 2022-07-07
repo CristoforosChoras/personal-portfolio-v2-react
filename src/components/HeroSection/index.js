f149295d68f28fc895d455df86167f116153ac13
CristoforosChoras <christopher.choras@yahoo.com>
2022-07-07 08:46:50 +0200

    bg video

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,30 +1,47 @@
 import React, { useState } from "react";
-import Video from "../../videos/video2.mp4";
-import { HeroContainer, HeroBg, VideoBg,HeroContent,HeroH1,HeroP,HeroBtnWrapper,ArrowForward,ArrowRight } from "./HeroElements";
+import Video from "../../videos/video3.mp4";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
 
 function HeroSection() {
-const [hover,setHover] = useState(false)
+  const [hover, setHover] = useState(false);
 
-const onHover =()=>{
-    setHover(!hover)
-} 
-     
+  const onHover = () => {
+    setHover(!hover);
+  };
+
   return (
     <HeroContainer>
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
       <HeroContent>
-        <HeroH1>My name is Chri</HeroH1>
+        <HeroH1>My name is Chris</HeroH1>
         <HeroP>
           I am a web developer living germany,Berlin.currently i am studying in
           DCI,in this course i am trying to gain more knowledge to achieve my
           goals
         </HeroP>
         <HeroBtnWrapper>
-            <Button to="contactme" onMouseEnter={onHover} onMouseLeave={onHover}>
-                    contactme{hover? <ArrowForward/>:<ArrowRight/>}
-            </Button>
+          <Button
+            to="contactme"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
+          >
+            Contact Me{hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
         </HeroBtnWrapper>
       </HeroContent>
     </HeroContainer>
